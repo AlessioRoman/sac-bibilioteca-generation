a48a44a77497a4adb91f74b9d9dd357035d9deb8
Damianopipitone <damianopipitone33@gmail.com>
2023-06-05 15:51:26 +0200

    Added view create

---
+++ biblioteca-mvc/Controllers/LibriController.cs
@@ -0,0 +1,40 @@
+﻿using biblioteca_mvc.Database;
+using biblioteca_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace biblioteca_mvc.Controllers
+{
+    public class LibriController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (BookContext db = new BookContext())
+            {
+                List<BookModel> books = db.Books.ToList();
+
+                return View(books);
+            }
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(BookModel newBook)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newBook);
+            } else
+            {
+                using(BookContext db = new BookContext())
+                {
+                    db.Books.Add(newBook);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+        }
+    }
+}
